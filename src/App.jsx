23d78dd20182cacb6f4437ff213630ba31525865
mauroarms <mauro.armas14@gmail.com>
2024-01-24 20:35:42 -0300

    Se realizó el Hola Mundo en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Informacion from "./components/Informacion"
+
 function App() {
   
   return (
     <>
       <h1>Prueba</h1>
+      <Informacion></Informacion>
     </>
   )
 }
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,12 @@
+const Informacion = () => {
+  return (
+    <>
+      <h1>Soy Maurito</h1>
+      <section>
+        <h2>Hola mundo desde React!</h2>
+      </section>
+    </>
+  );
+};
+
+export default Informacion;
